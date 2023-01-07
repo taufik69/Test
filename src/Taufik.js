2d57e7220272f0bae614f46ea9163aade709e461
taufik69 <taufik35-2915@diu.edu.bd>
2023-01-08 02:46:52 +0600

    changing file of taufik.js . reduce some text and add just h1 text

--- src/Taufik.js
+++ src/Taufik.js
@@ -3,8 +3,7 @@ import React from "react";
 const Taufik = () => {
   return (
     <div>
-      Taufik is a good boy . he loves sorna but sorna cheat his. but he is too
-      happy because thats bloody women are not existinfg on her life
+      <h1>taufik islam</h1>
     </div>
   );
 };
