62b5f5ceedaaaa38079f8ee633d8a4d424d29ab9
taufik69 <taufik35-2915@diu.edu.bd>
2023-01-07 23:16:19 +0600

    this is all componet of day 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  let [frist, setfrist] = useState(0);
+  let [color, setcolor] = useState();
+  let HandleClick = (e) => {
+    setfrist(frist + 1);
+    console.log(frist);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={h1}>{frist}</h1>
+      <button style={btn} onClick={HandleClick}>
+        Click me
+      </button>
+
+      {/* no9thing fancy  */}
+      <h2>{color}</h2>
+    </>
   );
 }
-
+let h1 = {
+  fontSize: "100px",
+  textAlign: "center",
+  marginTop: "20px",
+};
+let btn = {
+  marginLeft: "920px",
+};
 export default App;
